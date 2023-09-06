550e7a2740978cc6bfc512063fbd9ed335452617
XIAXuanzheFrancis <3058634081@qq.com>
2023-09-06 17:08:18 +0200

    “init”

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h1 class="home">XIAHome</h1>
+    </div>
+
+  <el-row>
+    <el-button>Default</el-button>
+    <el-button type="primary" size = "large">Primary</el-button>
+    <el-button type="success" size = "small">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
+
+</template>
+
+<style>
+.home {
+  color: red;
+}
+</style>
+
